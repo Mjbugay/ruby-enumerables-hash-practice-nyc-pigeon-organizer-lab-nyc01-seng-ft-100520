37a63dfd785e81a873bf802d879a0db40647546e
Matt Bugay <mjbugay@yahoo.com>
2020-09-22 18:59:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,8 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(key, value), final_array|
+   value.each do |inner_key, names|
+     names.each do |name|
+       
+ end
+  
 end
